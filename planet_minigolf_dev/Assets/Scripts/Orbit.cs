f62a564752da9edf18813d63a1b69b4f47530c0d
JuppizuppiT <jonasturek@gmx.de>
2022-10-27 18:20:44 +0200

    Add asteroids

---
+++ planet_minigolf_dev/Assets/Scripts/Orbit.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject planet;
+    public float speed = 1.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OrbitAround();
+    }
+
+    void OrbitAround()
+    {
+        transform.RotateAround(planet.transform.position, Vector3.forward, speed * Time.deltaTime);
+    }
+}
