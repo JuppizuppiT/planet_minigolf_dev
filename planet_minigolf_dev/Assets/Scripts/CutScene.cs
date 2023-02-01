8da0e6e255ff6bd71f6f2f7cd581750bebb3f181
JuppizuppiT <jonasturek@gmx.de>
2023-02-01 22:51:41 +0100

    add cutscene

---
+++ planet_minigolf_dev/Assets/Scripts/CutScene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutScene : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject mate;
+
+    float speed = 3.0f; //how fast it shakes
+    float amount = 0.1f; //how much it shakes
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        player.transform.position = new Vector3(- Mathf.Sin(Time.time * speed) * 0.7f * amount - 4, 0, 0);
+        mate.transform.position = new Vector3(Mathf.Sin(Time.time * speed) * amount + 4, 0, 0);
+    }
+}
